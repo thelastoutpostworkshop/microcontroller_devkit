0ecf2c4dad5b7bc92c7336165a11556e5ea7aa00
charles <cgiguere@iname.com>
2023-12-13 21:22:09 -0500

    colors

--- gpio_viewer/assets/script/webSocket.js
+++ gpio_viewer/assets/script/webSocket.js
@@ -1,4 +1,27 @@
 var ws;
+const colors = [
+  "#00ff00", // Green
+  "#1fff00",
+  "#3eff00",
+  "#5dff00",
+  "#7cff00",
+  "#9bff00",
+  "#baff00",
+  "#d9ff00",
+  "#f8ff00",
+  "#ffff00", // Yellow
+  "#ffef00",
+  "#ffdf00",
+  "#ffcf00",
+  "#ffbf00",
+  "#ffaf00",
+  "#ff9f00",
+  "#ff8f00",
+  "#ff7f00",
+  "#ff6f00",
+  "#ff0000"  // Red
+];
+
 function initWebSocket() {
   ws = new WebSocket("ws://" + window.location.hostname + ":" + serverPort + "/ws");
   ws.onmessage = function (event) {
@@ -11,18 +34,16 @@ function initWebSocket() {
 
 function setIndicatorColor(indicatorId, value) {
   const indicator = document.getElementById(indicatorId);
-  if (!indicator) return; // Exit if the indicator is not found
+  if (!indicator) return;
 
   // Ensure the value is within the range [0, 256]
   value = Math.max(0, Math.min(value, 256));
 
-  // Calculate the red and green components based on the value
-  // The red component increases with the value, green decreases
-  const red = Math.round((value / 256) * 255);
-  const green = 255 - red;
+  // Determine the index for the color array (20 segments)
+  const index = Math.floor((value / 256) * (colors.length - 1));
 
   // Set the background color
-  indicator.style.backgroundColor = `rgb(${red}, ${green}, 0)`;
+  indicator.style.backgroundColor = colors[index];
 }
 
 window.addEventListener("load", initWebSocket);
