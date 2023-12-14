7ff87740947f9d7c7c09114fc20f1ac6ad59ce0f
charles <cgiguere@iname.com>
2023-12-13 20:53:13 -0500

    setIndicatorColor

--- gpio_viewer/assets/script/webSocket.js
+++ gpio_viewer/assets/script/webSocket.js
@@ -4,17 +4,25 @@ function initWebSocket() {
   ws.onmessage = function (event) {
     var states = JSON.parse(event.data);
     for (var gpio in states) {
-      var gpioElement = document.getElementById("gpio" + gpio);
-      if (gpioElement) {
-        if (states[gpio]) {
-          gpioElement.className = "indicator-on";
-          gpioElement.innerHTML = "";
-        } else {
-          gpioElement.className = "indicator-off";
-          gpioElement.innerHTML = "";
-        }
-      }
+      setIndicatorColor("gpio" + gpio, states[v]);
     }
   };
 }
+
+function setIndicatorColor(indicatorId, value) {
+  const indicator = document.getElementById(indicatorId);
+  if (!indicator) return; // Exit if the indicator is not found
+
+  // Ensure the value is within the range [0, 256]
+  value = Math.max(0, Math.min(value, 256));
+
+  // Calculate the red and green components based on the value
+  // The red component increases with the value, green decreases
+  const red = Math.round((value / 256) * 255);
+  const green = 255 - red;
+
+  // Set the background color
+  indicator.style.backgroundColor = `rgb(${red}, ${green}, 0)`;
+}
+
 window.addEventListener("load", initWebSocket);
