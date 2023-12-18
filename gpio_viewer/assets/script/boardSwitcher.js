88196efc9ce27a5e7ab6e37653fa0400b6effb52
charles <cgiguere@iname.com>
2023-12-17 19:02:25 -0500

    toggleValuesVisibility

--- gpio_viewer/assets/script/boardSwitcher.js
+++ gpio_viewer/assets/script/boardSwitcher.js
@@ -20,8 +20,22 @@ async function initializeMenu() {
       headerElement.insertAdjacentHTML("afterbegin", menuHtml); // Prepend the content to the header element
       await populateMenu(); // Ensure this is called after the HTML snippet is added
       await switchBoard();
+      document.getElementById("toggleValues").addEventListener("change", function () {
+        toggleValuesVisibility();
+      });
     }
   }
+}
+
+function toggleValuesVisibility() {
+  const values = document.querySelectorAll('.value');
+  values.forEach(value => {
+      if (value.style.display === 'none') {
+          value.style.display = ''; // Resets to default display value
+      } else {
+          value.style.display = 'none';
+      }
+  });
 }
 
 async function loadBoardsData() {
@@ -65,40 +79,38 @@ async function populateMenu() {
 }
 
 async function switchBoard() {
-    const selector = document.getElementById("boardSelector");
-    const selectedBoardName = selector.value;
-  
-    // const boardsData = await loadBoardsData();
-    // console.log("Loaded boards data:", boardsData); // Debugging line
-  
-    const board = boardsData.find((b) => b.name === selectedBoardName);
-    console.log("Selected board:", board); // Debugging line
-  
-    if (board) {
-      document.getElementById("defaultStyleSheet").href = "css/default.css";
-      document.getElementById("boardStyleSheet").href = board.css;
-      document.getElementById("boardImage").src = board.image;
-  
-      // Load and replace only the indicators div
-      try {
-        const pinsHtml = await loadHtmlSnippet(board.pins);
-        const oldIndicatorsElement = document.getElementById("indicators");
-        if (pinsHtml && oldIndicatorsElement) {
-          const newIndicatorsElement = document.createElement('div');
-          newIndicatorsElement.innerHTML = pinsHtml;
-  
-          oldIndicatorsElement.replaceWith(newIndicatorsElement);
-        } else {
-          console.error("Pins HTML or indicators element not found");
-        }
-      } catch (error) {
-        console.error(`Error loading pins HTML from ${board.pins}:`, error);
+  const selector = document.getElementById("boardSelector");
+  const selectedBoardName = selector.value;
+
+  // const boardsData = await loadBoardsData();
+  // console.log("Loaded boards data:", boardsData); // Debugging line
+
+  const board = boardsData.find((b) => b.name === selectedBoardName);
+  console.log("Selected board:", board); // Debugging line
+
+  if (board) {
+    document.getElementById("defaultStyleSheet").href = "css/default.css";
+    document.getElementById("boardStyleSheet").href = board.css;
+    document.getElementById("boardImage").src = board.image;
+
+    // Load and replace only the indicators div
+    try {
+      const pinsHtml = await loadHtmlSnippet(board.pins);
+      const oldIndicatorsElement = document.getElementById("indicators");
+      if (pinsHtml && oldIndicatorsElement) {
+        const newIndicatorsElement = document.createElement("div");
+        newIndicatorsElement.innerHTML = pinsHtml;
+
+        oldIndicatorsElement.replaceWith(newIndicatorsElement);
+      } else {
+        console.error("Pins HTML or indicators element not found");
       }
-    } else {
-      console.error(`Board not found for name: ${selectedBoardName}`);
+    } catch (error) {
+      console.error(`Error loading pins HTML from ${board.pins}:`, error);
     }
+  } else {
+    console.error(`Board not found for name: ${selectedBoardName}`);
   }
-  
-  
+}
 
 window.onload = initializeMenu;
