d2429e871ee475e24d5dc28c37fe8af1e2f45de7
charles <cgiguere@iname.com>
2023-12-21 20:20:08 -0500

    constraints

--- camera-app/app.js
+++ camera-app/app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: { facingMode: "environment" }, audio: false };
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
